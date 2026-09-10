0e3da93480078df094e1ca135afa6a74b96a982f
Am0rr <itsaizel468@gmail.com>
2026-09-10 17:23:38 +0300

    feat(dal): implement UserRepository

--- backend/US.DAL/Interfaces/IUserRepository.cs
+++ backend/US.DAL/Interfaces/IUserRepository.cs
@@ -4,5 +4,5 @@ namespace US.DAL.Interfaces;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
 }
---
+++ backend/US.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+using US.DAL.Entities;
+using US.DAL.Interfaces;
+using US.DAL.Persistence;
+
+namespace US.DAL.Repositories;
+
+public class UserRepository(AppDbContext context
+    ) : BaseRepository<User>(context), 
+    IUserRepository
+{
+    public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await DbSet.FindAsync([email], cancellationToken);
+    }
+}
