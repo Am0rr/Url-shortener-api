d3ff3d488435d505c714f9e6f174fd1ba2e8d61b
Am0rr <itsaizel468@gmail.com>
2026-09-10 17:19:32 +0300

    feat(dal); implement BaseRepository

--- backend/US.DAL/Interfaces/IBaseRepository.cs
+++ backend/US.DAL/Interfaces/IBaseRepository.cs
@@ -7,8 +7,8 @@ public interface IBaseRepository<T> where T : BaseEntity
 {
     void Add(T item);
     void Update(T item);
-    void Delete(int id);
-    Task<T> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    void Delete(T item);
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
     Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
 }
---
+++ backend/US.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using US.DAL.Entities;
+using US.DAL.Interfaces;
+using US.DAL.Persistence;
+using System.Linq.Expressions;
+
+namespace US.DAL.Repositories;
+
+public class BaseRepository<T>(
+    AppDbContext context) 
+    : IBaseRepository<T> where T : BaseEntity
+{
+    protected readonly DbSet<T> DbSet = context.Set<T>();
+    
+    public void Add(T item)
+    {
+        DbSet.Add(item);
+    }
+
+    public void Update(T item)
+    {
+        DbSet.Update(item);
+    }
+
+    public void Delete(T item)
+    {
+        DbSet.Remove(item);
+    }
+
+    public virtual async Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return await DbSet.FindAsync([id], cancellationToken);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return await DbSet.AsNoTracking().ToListAsync(cancellationToken);
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken = default)
+    {
+        return await DbSet.AnyAsync(predicate, cancellationToken);
+    }
+}
