95417d04a2c7e3baad91c114af43c100daaab07d
Am0rr <itsaizel468@gmail.com>
2026-09-10 16:33:08 +0300

    feat(dal): create IUserRepository interface

---
+++ backend/US.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+using US.DAL.Entities;
+
+namespace US.DAL.Interfaces;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+}
