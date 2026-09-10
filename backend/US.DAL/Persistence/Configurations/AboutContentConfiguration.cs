d62d837389be96cfbf5dcab82f8851513804b949
Am0rr <itsaizel468@gmail.com>
2026-09-10 13:57:19 +0300

    feat(dal): implement AboutContentConfiguration

---
+++ backend/US.DAL/Persistence/Configurations/AboutContentConfiguration.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using US.DAL.Entities;
+
+namespace US.DAL.Persistence.Configurations;
+
+public class AboutContentConfiguration : IEntityTypeConfiguration<AboutContent>
+{
+    public void Configure(EntityTypeBuilder<AboutContent> builder)
+    {
+        builder.ToTable("AboutContents");
+        
+        builder.HasKey(a => a.Id);
+        
+        builder.Property(a => a.CreatedAt).IsRequired();
+        builder.Property(a => a.Text).IsRequired();
+
+        builder.HasOne(a => a.LastModifiedBy)
+            .WithMany()
+            .HasForeignKey(a => a.LastModifiedByUserId)
+            .OnDelete(DeleteBehavior.Restrict)
+            .IsRequired();
+    }
+}
