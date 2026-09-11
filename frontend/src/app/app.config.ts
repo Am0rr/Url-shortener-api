e93b725d26390626e5b898e55fd4e3ec11cb54be
Am0rr <itsaizel468@gmail.com>
2026-09-11 20:14:28 +0300

    feat(frontend): add api base url const to app.config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
+export const API_BASE_URL = 'https://localhost:7000/api';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
