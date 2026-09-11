287878c85ee04e79feaec935cfffb67db7568b82
Am0rr <itsaizel468@gmail.com>
2026-09-11 20:31:44 +0300

    feat(frontend): implrmrny auth.interceptor

---
+++ frontend/src/app/core/interceptors/auth.interceptors.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { API_BASE_URL } from '../../app.config';
+import { AuthService } from '../services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.token;
+
+  if (!token || !req.url.startsWith(API_BASE_URL)) {
+    return next(req);
+  }
+
+  return next(
+    req.clone({
+      setHeaders: { Authorization: `Bearer ${token}` },
+    }),
+  );
+};
